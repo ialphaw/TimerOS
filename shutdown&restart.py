8f6955a544efe4865a4a83c47afef4617dfec05a
ialphaw <amirabbas.vaziri@gmail.com>
2020-10-14 11:37:47 +0330

    Shutdown and Restart Computer with timer

---
+++ shutdown&restart.py
@@ -0,0 +1,18 @@
+# Python Program - Shutdown and Restart Computer
+
+import os;
+import time;
+print("1. Shutdown Computer");
+print("2. Restart Computer");
+print("3. Exit");
+choice = int(input("\nEnter your choice: "));
+sleepTime = int(input('Enter remaining time(seconds): '))
+if(choice>=1 and choice<=2):
+    if choice == 1:
+        time.sleep(sleepTime)
+        os.system("shutdown /s /t 1");
+    else:
+        time.sleep(sleepTime)
+        os.system("shutdown /r /t 1");
+else:
+    exit();
