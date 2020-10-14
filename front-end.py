6348171c57888bfd365003b4e3c1fb8b18dd46f2
ialphaw <amirabbas.vaziri@gmail.com>
2020-10-14 15:28:19 +0330

    objectproperty sync and it worked!

--- front-end.py
+++ front-end.py
@@ -1,15 +1,34 @@
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
 
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton, MDRoundFlatButton
+
+
+class Manager(ScreenManager):
+    pass
+
+
+class Main(Screen):
+    reqText = ObjectProperty(None)
+    timeText = ObjectProperty(None)
+
+    def btn(self):
+        print(self.reqText.text)
+        print(self.timeText.text)
+
+
+class MyLayout(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Gray"
-        return Builder.load_file('KV.kv')
+        return Builder.load_file('main.kv')
 
 
 MainApp().run()
