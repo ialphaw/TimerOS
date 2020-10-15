dceb6d5bb1b3c499d10a86e4b6faaf987bba3fbe
ialphaw <amirabbas.vaziri@gmail.com>
2020-10-15 12:43:49 +0330

    a little more changes and finilizng

--- activity.py
+++ activity.py
@@ -4,10 +4,10 @@ import time
 
 def reqActivity(req, timex):
     if req == 'Shutdown':
-        # os.system("shutdown /s /t 1")
-        print('your system is shutting down')
+        os.system("shutdown /s /t 1")
+        # print('your system is shutting down')
     else:
-        print('your system is restarting')
-        # os.system("shutdown /r /t 1")
+        # print('your system is restarting')
+        os.system("shutdown /r /t 1")
 
 
